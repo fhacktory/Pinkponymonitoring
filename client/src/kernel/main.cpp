dcfe3f84ab6ec47fb17b70ed60535456cf301f41
camill_n <camill_n@localhost.localdomain>
2014-10-11 14:06:38 +0200

    First commit =D

---
+++ client/src/kernel/main.cpp
@@ -0,0 +1,19 @@
+//
+// main.cpp for ppm in /home/camill_n/pinkponymonitoring/client
+//
+// Made by camill_n
+// Login   <camill_n@epitech.net>
+//
+// Started on  Sat Oct 11 13:41:31 2014 camill_n
+// Last update Sat Oct 11 13:52:34 2014 camill_n
+//
+
+#include <iostream>
+
+using namespace std;
+
+int	main(int ac, char **av)
+{
+  cout << "yeeeeeeeeeeees" << endl;
+  return (0);
+}
