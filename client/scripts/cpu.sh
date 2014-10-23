a500233c24a34be3afab4fbf4fc13c4d55142691
TristanCombret <tristan.combret@epitech.eu>
2014-10-23 18:09:39 +0200

    Update cpu.sh

--- client/scripts/cpu.sh
+++ client/scripts/cpu.sh
@@ -7,29 +7,29 @@
 ./scripts/src/init.sh
 
 # CHECK
-command -v cat 2>&1 >> /dev/null 
+command -v cat 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: cat"
     exit 1
 fi
-command -v grep 2>&1 >> /dev/null 
+command -v grep 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: grep"
     exit 1
 fi
-command -v tr 2>&1 >> /dev/null 
+command -v tr 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: tr"
     exit 1
 fi
-command -v cut 2>&1 >> /dev/null 
+command -v cut 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: cut"
     exit 1
 fi
 
 # EXEC
-id_percent=`cat scripts/src/top.data | grep "%Cpu(s)" | tr -s ' ' | cut -d ' ' -f 8`
+id_percent=`cat scripts/src/top.data | grep "Cpu(s)" | tr -s ' ' | cut -d ' ' -f 8`
 id_percent_round=`echo $id_percent | cut -d '.' -f 1`
 utilisation_percent=$((100 - $id_percent_round))
 echo $utilisation_percent
