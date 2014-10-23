4401581ec992f9ac81390dd3bf065d17a41b60b9
TristanCombret <tristan.combret@epitech.eu>
2014-10-23 18:10:30 +0200

    Update la.sh

--- client/scripts/la.sh
+++ client/scripts/la.sh
@@ -7,27 +7,27 @@
 ./scripts/src/init.sh
 
 # CHECK
-command -v cat 2>&1 >> /dev/null 
+command -v cat 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: cat"
     exit 1
 fi
-command -v grep 2>&1 >> /dev/null 
+command -v grep 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: grep"
     exit 1
 fi
-command -v sed 2>&1 >> /dev/null 
+command -v sed 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: sed"
     exit 1
 fi
-command -v cut 2>&1 >> /dev/null 
+command -v cut 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: cut"
     exit 1
 fi
-command -v tr 2>&1 >> /dev/null 
+command -v tr 2>&1 >> /dev/null
 if [[ $? -ne 0 ]]; then
     echo "COMMAND NOT FOUND: tr"
     exit 1
@@ -35,5 +35,5 @@ fi
 
 
 # EXEC
-cat scripts/src/top.data | grep 'top -' | tr -s ' ' | cut -d ' ' -f 11 | tr ',' '.' | cut -d '.' -f 1-2
+cat scripts/src/top.data | grep 'top -' | tr -s ' ' | cut -d ' ' -f 12 | tr ',' '.' | cut -d '.' -f 1-2
 exit 0
